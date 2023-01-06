105077b56cb4e8e52869df0a681f1e6db187c8e8
casm3 <casm3@cin.ufpe.br>
2023-01-06 14:49:11 -0300

    active directory

--- active_directory.py
+++ active_directory.py
@@ -20,7 +20,7 @@ class Group(object):
         return self.name
 
 
-def is_user_in_group(user, group):
+def is_user_in_group(user, groups):
     """
     Return True if user is in the group, False otherwise.
 
@@ -28,12 +28,13 @@ def is_user_in_group(user, group):
       user(str): user name/id
       group(class:Group): group to check user membership against
     """
-    if user in group.get_users():
+    if user in groups.get_users() and type(groups) == Group:
         return True
-    if group.get_groups():
-        return is_user_in_group(user, group.get_groups())
-    else:
-        return False
+
+    for group in groups.get_groups():
+        if user in group.get_users():
+            return True
+    return False
 
 
 # Add your own test cases: include at least three test cases
@@ -44,10 +45,6 @@ def is_user_in_group(user, group):
 parent = Group("parent")
 child = Group("child")
 brother = Group("brother")
-sub_child = Group("subchild")
-
-sub_child_user = "sub_child_user"
-sub_child.add_user(sub_child_user)
 
 child_user = "child_user"
 child.add_user(child_user)
@@ -55,15 +52,12 @@ child.add_user(child_user)
 brother_user = "brother_user"
 brother.add_user(brother_user)
 
-child.add_group(sub_child)
 parent.add_group(child)
 parent.add_group(brother)
 
 # Test Case 1
-assert is_user_in_group("sub_child_user", sub_child)
-# # Test Case 2
-assert is_user_in_group("fail_test_case", sub_child) is False
-# # Test Case 3
-assert is_user_in_group("sub_child_user", parent)
-assert is_user_in_group("sub_child_user", child)
-assert is_user_in_group("brother_user", parent)
+assert is_user_in_group("child_user", child)
+# Test Case 2
+assert is_user_in_group("fail_test_case", brother) is False
+# Test Case 3
+assert is_user_in_group("child_user", parent)
